8569f058306e38c689098a545dcc9932e870136e
Alex Berardi <alexmberardi@gmail.com>
2018-08-07 19:51:14 -0400

    Adding hardcoded string for fallback local db url

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+print(os.environ.get('DATABASE_URL'))
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost/survivor_pool'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
