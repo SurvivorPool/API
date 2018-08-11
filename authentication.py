5c07e069b1eac6956861ec268785d63a06ddb9f2
Alex Berardi <alexmberardi@gmail.com>
2018-08-11 00:18:06 -0400

    Moving firebase config into environment vars.

--- authentication.py
+++ authentication.py
@@ -2,14 +2,15 @@ from functools import wraps
 from flask import request
 import firebase_admin
 from firebase_admin import auth, credentials
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+import os
 
-cred = credentials.Certificate('serviceAccountKey.json')
-default_app = firebase_admin.initialize_app(cred)
+default_app = firebase_admin.initialize_app()
 
 def login_required(f):
     @wraps(f)
     def decorated_func(*args, **kwargs):
-        #print(request.headers['auth'])
         try:
             print(auth.verify_id_token(request.headers['auth']))
             auth.verify_id_token(request.headers['auth'])
