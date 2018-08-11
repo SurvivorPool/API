98812fdd583adc03df3a8a301b9630d426218f95
Alex Berardi <alexmberardi@gmail.com>
2018-08-11 01:03:13 -0400

    Authentication fix

--- authentication.py
+++ authentication.py
@@ -6,7 +6,29 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 import os
 
-default_app = firebase_admin.initialize_app()
+cert = firebase_admin.credentials.Certificate({
+    "type":
+    "service_account",
+    "project_id":
+    os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id":
+    os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key":
+    os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email":
+    os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id":
+    os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri":
+    os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri":
+    os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url":
+    os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url":
+    os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+})
+default_app = firebase_admin.initialize_app(cert)
 
 def login_required(f):
     @wraps(f)
@@ -17,5 +39,4 @@ def login_required(f):
             return f(*args, **kwargs)
         except:
             return {'message': 'Unable to authenticate'}, 403
-        #print(args)
     return decorated_func
