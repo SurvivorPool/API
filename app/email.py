f32919dbe3f0986780388b103b1cc484d39e2736
Alex Berardi <alexmberardi@gmail.com>
2018-08-20 21:20:22 -0400

    Adding files for emailing and removing survivorpool from requirements.txt

---
+++ app/email.py
@@ -0,0 +1,16 @@
+from flask_mail import Message
+from flask import render_template
+from app import mail
+from models import UserModel
+
+
+def send_email(subject, sender, recipients):
+    msg = Message(subject, sender=sender, recipients=recipients)
+
+    all_users = UserModel.get_all_users()
+
+    for user in all_users:
+        msg.body = render_template('league_lost.txt', user=user)
+        msg.html = render_template('league_lost.html', user=user)
+        mail.send(msg)
+    return
