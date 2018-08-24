2455de8dd38ee0a11487971b53c84359de1dbb29
Alex Berardi <alexmberardi@gmail.com>
2018-08-24 19:49:09 -0400

    adding admin to nfl teams endpoint

--- resources/nfl_team.py
+++ resources/nfl_team.py
@@ -1,5 +1,6 @@
 from flask_restplus import Resource
 from controllers.nfl_team import NFLTeamController
+from authentication import admin_required
 
 
 class nflTeam(Resource):
@@ -7,5 +8,6 @@ class nflTeam(Resource):
     def put(self):
         return self.populate_teams()
 
+    @admin_required
     def populate_teams(cls):
         return NFLTeamController.populate_teams()
