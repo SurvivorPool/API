b9a5616580d6d95299e9ad06a79a077e37a515bb
Alex Berardi <alexmberardi@gmail.com>
2018-08-28 23:40:33 -0400

    Moving emailing to a background thread

--- app/email.py
+++ app/email.py
@@ -1,12 +1,19 @@
 from flask_mail import Message
 from flask import render_template
-from app import mail
+from app import mail, app
 from models.user import UserModel
+from threading import Thread
 
 
 def send_email(subject, sender, user, team):
     msg = Message(subject, sender=sender, recipients=[user.email])
     msg.html = render_template('league_lost.html', user=user, team=team)
-    mail.send(msg)
+    #send_email_async(msg, app)
+    Thread(target=send_email_async, args=(app, msg)).start()
 
     return
+
+
+def send_email_async(app_inner, msg):
+    with app_inner.app_context():
+        mail.send(msg)
