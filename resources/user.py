d953708ce85fb105e67c34db121ab6a6ba157c3b
Alex Berardi <alexmberardi@gmail.com>
2018-08-30 22:31:35 -0400

    Default admin emails to admin users on registration.

--- resources/user.py
+++ resources/user.py
@@ -2,6 +2,9 @@ from flask_restplus import Resource, reqparse, fields
 import app
 from models.user import UserModel
 import authentication
+from dotenv import load_dotenv
+import os
+load_dotenv(verbose=True)
 
 api = app.api
 user_swagger = api.model('User', {
@@ -61,6 +64,10 @@ class User(Resource):
         data = self.parser.parse_args()
         user = UserModel(data['user_id'], data['full_name'], data['email'],
                          data['picture_url'])
+
+        if user.email in os.environ.get('ADMIN_EMAILS'):
+            user.is_admin = True
+
         try:
             user.upsert()
         except:
