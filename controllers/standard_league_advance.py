8a3170a106aa86cc05ba4581c63fa459d015e7d4
Alex Berardi <alexmberardi@gmail.com>
2018-09-25 21:51:05 -0400

    Adding completed bool to league model. commenting out advance week code for testing.

--- controllers/standard_league_advance.py
+++ controllers/standard_league_advance.py
@@ -26,16 +26,16 @@ class StandardLeagueAdvanceController:
                 deactivated_teams.append(active_team)
             else:
                 if pick.nfl_team_name in losing_nfl_teams:
-                    active_team.is_active = False
+                    #active_team.is_active = False
                     deactivated_teams.append(active_team)
                 else:
-                    active_team.streak += 1
+                    #active_team.streak += 1
                     advancing_teams.append(active_team)
-                active_team.upsert()
+                #active_team.upsert()
 
-        for team in deactivated_teams:
-            if team.user.receive_notifications:
-                send_email("Sorry, you've been eliminated", Config.MAIL_USERNAME, team.user, team)
+        # for team in deactivated_teams:
+        #     if team.user.receive_notifications:
+        #         send_email("Sorry, you've been eliminated", Config.MAIL_USERNAME, team.user, team)
 
         return {
             'deactivated_teams': deactivated_teams,
--- models/league.py
+++ models/league.py
@@ -17,6 +17,7 @@ class LeagueModel(db.Model):
     league_type_id = db.Column(db.Integer, db.ForeignKey('league_types.league_type_id'),
                                default=LeagueTypes.STANDARD.value, server_default=str(LeagueTypes.STANDARD.value))
     start_week = db.Column(db.Integer, nullable=False, default=1, server_default="1")
+    completed = db.Column(db.Integer, nullable=False, default=0, server_default="0")
     teams = db.relationship("PlayerTeamModel", order_by="desc(PlayerTeamModel.is_active), desc(PlayerTeamModel.streak)")
     league_type = db.relationship("LeagueTypeModel")
 
