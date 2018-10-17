3940024c0a4fdc3958386b9717459693e780a9b8
Alex Berardi <alexmberardi@gmail.com>
2018-10-17 19:01:40 -0400

    commenting out update code and update games temporarily

--- controllers/standard_league_advance.py
+++ controllers/standard_league_advance.py
@@ -23,16 +23,15 @@ class StandardLeagueAdvanceController:
             pick = PickModel.find_pick_by_week_and_team_id(week_num, active_team.team_id)
 
             if pick is None:
-                pass
-                #deactivated_teams.append(active_team)
+                deactivated_teams.append(active_team)
             else:
                 if pick.nfl_team_name in losing_nfl_teams:
-                    #active_team.is_active = False
+                    active_team.is_active = False
                     deactivated_teams.append(active_team)
                 else:
-                    #active_team.streak += 1
+                    active_team.streak += 1
                     advancing_teams.append(active_team)
-                # active_team.upsert()
+                active_team.upsert()
 
         # for team in deactivated_teams:
         #     if team.user.receive_notifications:
