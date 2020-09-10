e79cb7159ac49a1ee46550b97acb565accfca9a8
Alex Berardi <alexmberardi@gmail.com>
2020-09-10 17:05:07 -0400

    Readding auth middleware to games requests.

--- resources/game.py
+++ resources/game.py
@@ -5,12 +5,12 @@ import authentication
 
 class GamesList(Resource):
 
-    #@authentication.login_required
+    @authentication.login_required
     def get(self):
         return GameController.update_games()
 
 
 class AdminGames(Resource):
-    #@authentication.admin_required
+    @authentication.admin_required
     def put(self):
         return GameController.populate_games()
